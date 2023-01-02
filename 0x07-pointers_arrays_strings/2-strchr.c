b07108e1d0bd47b5b2cc910785cb6189c9287559
farid mustapha <khingfarid@yahoo.com>
2023-01-02 15:39:41 -0800

    done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,18 +10,17 @@
  */
 char *_strchr(char *s, char c)
 {
-		int a;
-		while (1)
-		{
-			a = *s++;
-			if (a == c)
-			{
-				return (s - 1);
-			}
-			if (a == 0)
-			{
-				return (NULL);
-			}
-		}
-
+int a;
+while (1)
+{
+a = *s++;
+if (a == c)
+{
+return (s - 1);
+}
+if (a == 0)
+{
+return (NULL);
+}
+}
 }
