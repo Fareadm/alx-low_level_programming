c6c4db439052d2d1661904c22b6922ef44fa9b2d
farid mustapha <khingfarid@yahoo.com>
2023-01-04 04:39:05 -0800

    corction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
-int _putchar(char c);
+int _putchar(char );
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
