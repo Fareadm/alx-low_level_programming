0e435d78dd0e8db0c85d4438cd52c4a774d7d1d0
farid mustapha <khingfarid@yahoo.com>
2023-01-10 15:08:06 -0800

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,42 +9,30 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, size;
-	char *arg;
-	size = 0;
-	k = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-		size++;
-		j++;
-		}
-		size++;
-		i++;
-	}
-	arg = malloc((sizeof(char) * size) + 1);
-	if (arg == NULL)
-		return (NULL);
-		i = 0;
-
-		while (i < ac)
-		{
-											j = 0;
-			while (av[i][j])
-			{
-				arg[k] = av[i][j];
-				j++;
-				k++;
-			}
-			arg[k] = '\n';
-			k++;
-			i++;
-		}
-		arg[k] = '\0';
-		return (arg);
+int i, n, k = 0, len = 0;
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+len++;
+}
+len += ac;
+str = malloc(sizeof(char) * len + 1);
+if (str == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+{
+for (n = 0; av[i][n]; n++)
+{
+str[k] = av[i][n];
+k++;
+}
+if (str[k] == '\0')
+{
+str[k++] = '\n';
+}
+}
+return (str);
 }
